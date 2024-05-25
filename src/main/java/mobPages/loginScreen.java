e9c4a9ffa4da3c58edf23ee849c31cde51833b02
rajyenekal <rajyenekal@gmail.com>
2024-05-25 15:22:59 +0530

    Web and api script updated

--- src/main/java/mobPages/loginScreen.java
+++ src/main/java/mobPages/loginScreen.java
@@ -5,7 +5,6 @@ import java.time.Duration;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.appium.java_client.AppiumDriver;
